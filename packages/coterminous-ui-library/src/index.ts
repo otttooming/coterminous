f25866b219a0e77b54652560480116b63979ed19
Ott Tooming <OttTooming@gmail.com>
2018-12-25 02:30:21 +0200

    Add webpack base build process for ui-lib

--- packages/coterminous-ui-library/src/index.ts
+++ packages/coterminous-ui-library/src/index.ts
@@ -7,20 +7,13 @@ export { rotate360 } from './common/styles/keyframes';
 export { Heading } from './components/Heading/Heading';
 export { Button } from './components/Button/Button';
 
-export { FormProvider, FormContext, FormState } from './components/Form/Form';
+export { FormProvider, FormContext } from './components/Form/Form';
 
-export {
-  withFormConsumer,
-  ExternalWithFormConsumerProps,
-} from './components/Form/withFormConsumer';
+export { withFormConsumer } from './components/Form/withFormConsumer';
 
 export { TextInputField } from './components/TextInput/TextInputField';
 
-export {
-  Checkbox,
-  CheckboxProps,
-  CheckboxType,
-} from './components/Checkbox/Checkbox';
+export { Checkbox } from './components/Checkbox/Checkbox';
 export { CheckboxField } from './components/Checkbox/CheckboxField';
 
 export { RadioButtonField } from './components/RadioButton/RadioButtonField';
@@ -28,6 +21,6 @@ export { RadioButtonField } from './components/RadioButton/RadioButtonField';
 export { Section } from './components/Section/Section';
 export { CSSIcons } from './components/Icons/Icons';
 
-export { Select, SelectItemProps } from './components/Select/Select';
+export { Select } from './components/Select/Select';
 export { SelectField } from './components/Select/SelectField';
 export { Popover } from './components/Popover/Popover';
