f9d5ea88ee8296a77084e2c803b78167afce6338
Ott Tooming <OttTooming@gmail.com>
2019-01-12 13:51:38 +0200

    Add graphql codegen for store-gw

---
+++ packages/coterminous-store-gw/src/generated-types.ts
@@ -0,0 +1,58 @@
+export type Maybe<T> = T | null;
+
+export enum CacheControlScope {
+  Public = 'PUBLIC',
+  Private = 'PRIVATE',
+}
+
+export type Upload = any;
+
+// ====================================================
+// Scalars
+// ====================================================
+
+// ====================================================
+// Types
+// ====================================================
+
+export interface Query {
+  hello?: Maybe<string>;
+
+  productsListing?: Maybe<ProductsListing>;
+}
+
+export interface ProductsListing {
+  cursor?: Maybe<string>;
+
+  pageInfo?: Maybe<PageInfo>;
+
+  edges?: Maybe<(Maybe<ProductNode>)[]>;
+}
+
+export interface PageInfo {
+  hasNextPage: boolean;
+
+  hasPreviousPage: boolean;
+
+  startCursor?: Maybe<string>;
+
+  endCursor?: Maybe<string>;
+}
+
+export interface ProductNode {
+  node?: Maybe<Product>;
+}
+
+export interface Product {
+  name?: Maybe<string>;
+
+  createdAt?: Maybe<string>;
+}
+
+// ====================================================
+// Arguments
+// ====================================================
+
+export interface ProductsListingQueryArgs {
+  cursor?: Maybe<string>;
+}
