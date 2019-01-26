af063fd34fe04e572aca76f677e89d9fcd968056
Ott Tooming <OttTooming@gmail.com>
2019-01-26 21:59:29 +0200

    Add GlobalStyle to Heading.stories

--- packages/coterminous-ui-lib/src/components/Heading/Heading.stories.tsx
+++ packages/coterminous-ui-lib/src/components/Heading/Heading.stories.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { theme, Heading } from '../../';
+import { GlobalStyle } from '../GlobalStyle/GlobalStyle';
+import { Section } from '../Section/Section';
 
-storiesOf('Heading', module)
-  .addDecorator(story => <div style={{ background: 'black' }}>{story()}</div>)
-  .add('Heading', () => (
-    <>
-      <Heading.H1>Heading</Heading.H1>
-      <Heading.H2>Heading</Heading.H2>
-      <Heading.H3>Heading</Heading.H3>
-      <Heading.H4>Heading</Heading.H4>
-      <Heading.H5>Heading</Heading.H5>
-      <Heading.H6>Heading</Heading.H6>
-    </>
-  ));
+storiesOf('Heading', module).add('Heading', () => (
+  <Section>
+    <GlobalStyle />
+
+    <Heading.H1>Heading</Heading.H1>
+    <Heading.H2>Heading</Heading.H2>
+    <Heading.H3>Heading</Heading.H3>
+    <Heading.H4>Heading</Heading.H4>
+    <Heading.H5>Heading</Heading.H5>
+    <Heading.H6>Heading</Heading.H6>
+  </Section>
+));
