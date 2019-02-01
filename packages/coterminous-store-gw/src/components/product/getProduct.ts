46a4f1d1ddc3f44eb011563ff5caecab4385df66
Ott Tooming <OttTooming@gmail.com>
2019-02-01 16:46:04 +0200

    Fix getProduct resolver not sending id and name fields

--- packages/coterminous-store-gw/src/components/product/getProduct.ts
+++ packages/coterminous-store-gw/src/components/product/getProduct.ts
@@ -1,14 +1,27 @@
 import { getProduct as getWCProduct } from '@coterminous/wp-lib';
+import { Product as WCProduct } from '@coterminous/wp-lib/dist/components/getProduct/getProduct';
 import { Product, ProductQueryArgs } from '../../codegen/types';
+import { requestLimiter } from './resolver';
 
 export async function getProduct({
   id,
 }: ProductQueryArgs): Promise<Product | null> {
-  const response = await getWCProduct(id);
+  const response: WCProduct = await requestLimiter.schedule(() =>
+    getWCProduct(id),
+  );
 
   if (!response) {
     return null;
   }
 
-  return response;
+  const {
+    product: { id: wcId, name },
+  } = response;
+
+  const product: Product = {
+    id: wcId,
+    name,
+  };
+
+  return product;
 }
