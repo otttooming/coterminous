5d3c0c47fb0086022ac6d13ced6f8225416a5531
Ott Tooming <OttTooming@gmail.com>
2019-03-12 00:06:50 +0200

    Remove old global styling from RadioButton stories

--- packages/coterminous-ui-lib/src/components/RadioButton/RadioButton.stories.tsx
+++ packages/coterminous-ui-lib/src/components/RadioButton/RadioButton.stories.tsx
@@ -3,28 +3,26 @@ import { storiesOf } from '@storybook/react';
 import { FormProvider, Heading, Section, RadioButtonField } from '../../';
 import StorybookWrapper from '../../common/storybookHelpers/StorybookWrapper';
 
-storiesOf('RadioButton', module)
-  .addDecorator(story => <div style={{ background: '#fff' }}>{story()}</div>)
-  .add('RadioButtonField', () => (
-    <StorybookWrapper>
-      <FormProvider>
-        <Section>
-          <Heading.H1>RadioButtonField</Heading.H1>
+storiesOf('RadioButton', module).add('RadioButtonField', () => (
+  <StorybookWrapper>
+    <FormProvider>
+      <Section>
+        <Heading.H1>RadioButtonField</Heading.H1>
 
-          <RadioButtonField
-            name="test_field"
-            label="Label title"
-            inputLabel="Input label"
-            value={{ test: 'value' }}
-          />
+        <RadioButtonField
+          name="test_field"
+          label="Label title"
+          inputLabel="Input label"
+          value={{ test: 'value' }}
+        />
 
-          <RadioButtonField
-            name="test_field"
-            label="Label title 2"
-            inputLabel="Input label 2"
-            value="Test 1"
-          />
-        </Section>
-      </FormProvider>
-    </StorybookWrapper>
-  ));
+        <RadioButtonField
+          name="test_field"
+          label="Label title 2"
+          inputLabel="Input label 2"
+          value="Test 1"
+        />
+      </Section>
+    </FormProvider>
+  </StorybookWrapper>
+));
