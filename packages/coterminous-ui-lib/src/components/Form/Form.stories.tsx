466f8677cba584c153c9411c514c2c67b7db288a
Ott Tooming <OttTooming@gmail.com>
2019-03-12 01:47:54 +0200

    Refactor Form stories to use StorybookWrapper

--- packages/coterminous-ui-lib/src/components/Form/Form.stories.tsx
+++ packages/coterminous-ui-lib/src/components/Form/Form.stories.tsx
@@ -1,51 +1,46 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import {
-  GlobalStyle,
   FormProvider,
-  theme,
   Button,
   Heading,
   Section,
-  CheckboxField,
   FormContext,
   TextInputField,
 } from '../../';
+import StorybookWrapper from '../../common/storybookHelpers/StorybookWrapper';
 
-storiesOf('Form', module)
-  .addDecorator(story => <div style={{ background: '#fff' }}>{story()}</div>)
-  .add('Submit', () => (
-    <>
-      <GlobalStyle />
-      <FormProvider>
-        <Section>
-          <Heading.H1>TextInputField</Heading.H1>
+storiesOf('Form', module).add('Submit', () => (
+  <StorybookWrapper>
+    <FormProvider>
+      <Section>
+        <Heading.H1>TextInputField</Heading.H1>
 
-          <TextInputField.Input name="test_field" label="Label title" />
-          <TextInputField.TextArea
-            name="test_field_textarea"
-            label="Label title"
-            isRequired={true}
-          />
-        </Section>
+        <TextInputField.Input name="test_field" label="Label title" />
+        <TextInputField.TextArea
+          name="test_field_textarea"
+          label="Label title"
+          isRequired={true}
+        />
+      </Section>
 
-        <Section>
-          <Heading.H1>Submit form</Heading.H1>
+      <Section>
+        <Heading.H1>Submit form</Heading.H1>
 
-          <FormContext.Consumer>
-            {props => (
-              <Button
-                onClick={() => {
-                  /* tslint:disable */
-                  console.log('1', props.actions.submit());
-                  /* tslint:enable */
-                }}
-              >
-                Submit
-              </Button>
-            )}
-          </FormContext.Consumer>
-        </Section>
-      </FormProvider>
-    </>
-  ));
+        <FormContext.Consumer>
+          {props => (
+            <Button
+              onClick={() => {
+                /* tslint:disable */
+                console.log('1', props.actions.submit());
+                /* tslint:enable */
+              }}
+            >
+              Submit
+            </Button>
+          )}
+        </FormContext.Consumer>
+      </Section>
+    </FormProvider>
+  </StorybookWrapper>
+));
