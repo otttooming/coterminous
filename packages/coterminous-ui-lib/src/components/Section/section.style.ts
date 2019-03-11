6cebf4864ebb4135631ffe4c0b3a9f1b455ff4ce
Ott Tooming <OttTooming@gmail.com>
2019-03-12 01:45:39 +0200

    Refactor Section margin to top

--- packages/coterminous-ui-lib/src/components/Section/section.style.ts
+++ packages/coterminous-ui-lib/src/components/Section/section.style.ts
@@ -4,9 +4,13 @@ import { css } from 'styled-components';
 const common = css`
   background-color: ${theme.background.section};
   border-radius: ${theme.border.radius.small};
-  margin-bottom: ${theme.spacing.normal};
+  margin-top: ${theme.spacing.normal};
   padding: ${theme.spacing.large};
   box-shadow: 0px 16px 25px 0px rgba(118, 88, 198, 0.1);
+
+  &:first-child {
+    margin-top: 0;
+  }
 `;
 
 export const sectionStyle = css`
