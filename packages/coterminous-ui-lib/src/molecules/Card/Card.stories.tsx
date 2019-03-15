0cfe6c97fd1d11f4e52042e8a241f5db864f6335
Ott Tooming <OttTooming@gmail.com>
2019-03-15 23:32:55 +0200

    Add list example of Card component

--- packages/coterminous-ui-lib/src/molecules/Card/Card.stories.tsx
+++ packages/coterminous-ui-lib/src/molecules/Card/Card.stories.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import StorybookWrapper from '../../common/storybookHelpers/StorybookWrapper';
 import Card from './';
-import { Heading, Button, ButtonSize } from '../../';
+import { Heading, Button, ButtonSize, Section } from '../../';
+import styled from 'styled-components';
 
 const image = {
   width: 1200,
@@ -24,12 +25,49 @@ const footer = (
   </>
 );
 
+const ListRoot = styled.ul`
+  display: grid;
+  grid-gap: 32px;
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+const ListItem = styled.li`
+  list-style: none;
+`;
+
 const stories = storiesOf('Card', module);
 stories
   .add('Default', () => {
     return (
       <StorybookWrapper>
         <Card footer={footer} image={image} />
+      </StorybookWrapper>
+    );
+  })
+  .add('List', () => {
+    return (
+      <StorybookWrapper>
+        <Heading.H1>Product list</Heading.H1>
+        <ListRoot>
+          <ListItem>
+            <Card footer={footer} image={image} />
+          </ListItem>
+          <ListItem>
+            <Card footer={footer} image={image} />
+          </ListItem>
+          <ListItem>
+            <Card footer={footer} image={image} />
+          </ListItem>
+          <ListItem>
+            <Card footer={footer} image={image} />
+          </ListItem>
+          <ListItem>
+            <Card footer={footer} image={image} />
+          </ListItem>
+          <ListItem>
+            <Card footer={footer} image={image} />
+          </ListItem>
+        </ListRoot>
       </StorybookWrapper>
     );
   })
