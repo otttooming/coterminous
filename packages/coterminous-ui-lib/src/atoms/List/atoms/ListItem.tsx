5c0a015f1923393ade06df17d8b8122a6458f229
Ott Tooming <OttTooming@gmail.com>
2019-03-16 16:59:46 +0200

    Add extended React.HTMLAttributes to ListItem

--- packages/coterminous-ui-lib/src/atoms/List/atoms/ListItem.tsx
+++ packages/coterminous-ui-lib/src/atoms/List/atoms/ListItem.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-interface ListItemProps {}
+interface ListItemProps extends React.HTMLAttributes<HTMLLIElement> {}
 
 const Wrapper = styled.li`
   list-style: none;
 `;
 
-const ListItem: React.FunctionComponent<ListItemProps> = ({ children }) => {
-  return <Wrapper>{children}</Wrapper>;
+const ListItem: React.FunctionComponent<ListItemProps> = ({
+  children,
+  ...restProps
+}) => {
+  return <Wrapper {...restProps}>{children}</Wrapper>;
 };
 
 export default ListItem;
