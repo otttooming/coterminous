9dbbaedce52e842c8f1453a8b0111d2aa76648ab
Ott Tooming <OttTooming@gmail.com>
2019-03-19 23:30:00 +0200

    Add landscape story to Image

--- packages/coterminous-ui-lib/src/components/Image/Image.stories.tsx
+++ packages/coterminous-ui-lib/src/components/Image/Image.stories.tsx
@@ -13,22 +13,38 @@ stories.add('Image', () => {
   const content = `My name is ${name}.`;
 
   const image = {
-    width: 1200,
-    height: 1200,
+    width: 360,
+    height: 640,
     aspectRatio: 100,
     sizes: [
       {
-        url: 'https://via.placeholder.com/1200',
-        width: 1200,
-        height: 1200,
+        url: 'https://dummyimage.com/360x640/333/aaa',
+        width: 360,
+        height: 640,
+      },
+    ],
+  };
+
+  const landscape = {
+    width: 640,
+    height: 360,
+    aspectRatio: 100,
+    sizes: [
+      {
+        url: 'https://dummyimage.com/640x360/333/aaa',
+        width: 640,
+        height: 360,
       },
     ],
   };
 
   return (
-    <StorybookWrapper style={{ width: 420 }}>
-      <Section>
+    <StorybookWrapper>
+      <Section style={{ width: 420 }}>
         <Image image={image} />
+      </Section>
+      <Section style={{ width: 420 }}>
+        <Image image={landscape} />
       </Section>
     </StorybookWrapper>
   );
