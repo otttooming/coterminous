765059e310bdfdd4ff392d8da20fa186e6b41829
Ott Tooming <OttTooming@gmail.com>
2019-03-24 22:03:18 +0200

    Add flat style for Vertical card component

--- packages/coterminous-ui-lib/src/molecules/Card/molecules/Vertical.tsx
+++ packages/coterminous-ui-lib/src/molecules/Card/molecules/Vertical.tsx
@@ -6,14 +6,17 @@ import { CardProps } from '../Card';
 export interface VerticalProps extends CardProps {}
 
 const Wrapper = styled.div`
-  background-color: ${theme.background.section};
-  border-radius: ${theme.border.radius.small};
-  box-shadow: 0px 16px 25px 0px rgba(118, 88, 198, 0.1);
+  /* background-color: ${theme.background.section}; */
+  /* box-shadow: 0px 16px 25px 0px rgba(118, 88, 198, 0.1); */
+`;
+
+const TopWrapper = styled.div`
   overflow: hidden;
+  border-radius: ${theme.border.radius.small};
 `;
 
 const Footer = styled.div`
-  padding: ${theme.spacing.large};
+  padding-top: ${theme.spacing.normal};
 `;
 
 class Vertical extends React.Component<VerticalProps, any> {
@@ -22,7 +25,11 @@ class Vertical extends React.Component<VerticalProps, any> {
 
     return (
       <Wrapper as={as} {...restProps}>
-        {!!image && <Image image={image} />}
+        {!!image && (
+          <TopWrapper>
+            <Image image={image} />
+          </TopWrapper>
+        )}
 
         {!!footer && <Footer>{footer}</Footer>}
       </Wrapper>
