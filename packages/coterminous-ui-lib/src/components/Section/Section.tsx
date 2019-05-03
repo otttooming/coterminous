0c66959baed13b0f40d9c3085018632e6b21780a
Ott Tooming <OttTooming@gmail.com>
2019-05-03 18:29:02 +0300

    Add withSpace to Section component

--- packages/coterminous-ui-lib/src/components/Section/Section.tsx
+++ packages/coterminous-ui-lib/src/components/Section/Section.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react';
 import { sectionStyle } from './section.style';
 import styled from 'styled-components';
+import {
+  WithSpaceProps,
+  withSpace,
+} from '../../common/styles/molecules/styledSystem';
 
-const Section = styled.section`
+type SectionProps = WithSpaceProps;
+
+const Wrapper = styled.section`
   ${sectionStyle};
+  ${withSpace}
 `;
+
+const Section: React.FC<SectionProps> = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
 
 export default Section;
--- packages/coterminous-ui-lib/src/components/Section/section.style.ts
+++ packages/coterminous-ui-lib/src/components/Section/section.style.ts
@@ -4,12 +4,7 @@ import { css } from 'styled-components';
 const common = css`
   background-color: ${theme.backgroundColor.secondary};
   border-radius: ${theme.borderRadius.s};
-  margin-top: ${theme.space.l};
   padding: ${theme.space.l};
-
-  &:first-child {
-    margin-top: 0;
-  }
 `;
 
 export const sectionStyle = css`
