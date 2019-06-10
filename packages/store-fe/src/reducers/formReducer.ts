7960e1d7a2fe316e78bda9e07d8651741b6bd8d7
Ott Tooming <OttTooming@gmail.com>
2019-06-10 23:58:36 +0300

    Remove redundant formReducer

--- packages/store-fe/src/reducers/formReducer.ts
+++
@@ -1,8 +0,0 @@
-export default (state = {}, action) => {
-  switch (action.type) {
-    case 'productListing':
-      return { ...state, productListing: action.productListing };
-    default:
-      return state;
-  }
-};
--- packages/store-fe/src/reducers/index.ts
+++ packages/store-fe/src/reducers/index.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-import formReducer from './formReducer';
 import productsListingLayerReducer, {
   ProductsListingLayerState,
 } from '../layers/productsListingLayer/productsListingLayerReducer';
 
 const rootReducer = combineReducers({
-  formReducer,
   productsListingLayerReducer,
 });
 
